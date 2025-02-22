0b4ea7231f70d743067220eb9f7b0ffaf1d8e44c
menyagah <menyagah27@gmail.com>
2025-02-22 10:58:42 +0300

    Add bugtrail routes for viewing and creation

--- main.go
+++ main.go
@@ -9,11 +9,21 @@ func home(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello from bugtrail"))
 }
 
+func bugtrailView(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("This is a view"))
+}
+
+func bugtrailCreate(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("This is the creation bit"))
+}
+
 
 
 func main(){
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/bugtrail/view", bugtrailView)
+	mux.HandleFunc("/bugtrail/create", bugtrailCreate)
 
 	log.Print("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
