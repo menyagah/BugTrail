21695f3a37922b1fbccaef873beab71aa6949693
menyagah <menyagah27@gmail.com>
2025-02-24 10:32:26 +0300

    Add bugtrailView with dynamic ID handling and error checking

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request){
@@ -10,7 +12,13 @@ func home(w http.ResponseWriter, r *http.Request){
 }
 
 func bugtrailView(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("This is a view"))
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	msg := fmt.Sprintf("Display a specific snippet with ID %d ", id)
+	w.Write([]byte(msg))
 }
 
 func bugtrailCreate(w http.ResponseWriter, r *http.Request){
@@ -19,11 +27,14 @@ func bugtrailCreate(w http.ResponseWriter, r *http.Request){
 
 
 
+
+
 func main(){
 	mux := http.NewServeMux()
 	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/bugtrail/view", bugtrailView)
+	mux.HandleFunc("/bugtrail/view/{id}", bugtrailView)
 	mux.HandleFunc("/bugtrail/create", bugtrailCreate)
+	
 
 	log.Print("starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
