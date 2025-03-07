0d34e99d948d601474b5a7a8a4aeb34e1a2a93dc
menyagah <menyagah27@gmail.com>
2025-03-07 20:31:57 +0300

    Add structured logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 
 func main(){
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	}))
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /bugtrail/view/{id}", bugtrailView)
 	mux.HandleFunc("GET /bugtrail/create", bugtrailCreate)
 	mux.HandleFunc("POST /bugtrail/create", bugtrailCreatePost)
 	
+	logger.Info("starting server", slog.String("addr", *addr))
 
-	log.Print("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	
+	err := http.ListenAndServe(*addr, mux)
+
+	logger.Error(err.Error())
+	os.Exit(1)
 }
 
